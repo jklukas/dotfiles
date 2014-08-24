205cb0ce13cd1c69ade8527e8ac92f7ccb1fae0f
Jeff Klukas <jeff@klukas.net>
2014-08-23 20:58:36 -0500

    Initial changes

---
+++ zsh/emacsdaemon.zsh
@@ -0,0 +1,6 @@
+export ALTERNATE_EDITOR=""
+export EDITOR=emacsclient
+
+alias  e='/Applications/Emacs.app//Contents/MacOS/bin/emacsclient -t'
+alias ec='/Applications/Emacs.app//Contents/MacOS/bin/emacsclient -c'
+alias ed='/Applications/Emacs.app//Contents/MacOS/Emacs --daemon'
